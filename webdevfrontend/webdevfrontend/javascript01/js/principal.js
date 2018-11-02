1c59b216bdc2d923db64e44715bd37eb7c0095a4
Carlos Melo <carlaofernandesdgt@gmail.com>
2018-11-01 22:58:59 -0300

    capitulo 4

--- webdevfrontend/webdevfrontend/javascript01/js/principal.js
+++ webdevfrontend/webdevfrontend/javascript01/js/principal.js
@@ -1,5 +1,37 @@
+var botaoAdicionario = document.querySelector("#adicionar-paciente");
+botaoAdicionario.addEventListener("click", function(event){
+    event.preventDefault();
+    console.log("fui clicado");
+    var formulario = document.querySelector("#form-adiciona");
+    var nome = formulario.nome.value;
+    var peso = formulario.peso.value;
+    var altura= formulario.altura.value;
+    var gordura = formulario.gordura.value;
+
+    tdNome = document.createElement("td");
+    tdPeso = document.createElement("td");
+    tdAltura = document.createElement("td");
+    tdGordura = document.createElement("td");
+
+    tdNome.textContent = nome;
+    tdPeso.textContent = peso;
+    tdAltura.textContent = altura;
+    tdGordura.textContent = gordura;
+
+    var pacienteTr = document.createElement("tr");
+    pacienteTr.appendChild(tdNome);
+    pacienteTr.appendChild(tdPeso);
+    pacienteTr.appendChild(tdAltura);
+    pacienteTr.appendChild(tdGordura);
+
+    document.querySelector("#tabela-pacientes").appendChild(pacienteTr);
+
+});
+
+
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
+
 var pacientes = document.querySelectorAll(".paciente");
 
 for (var i = 0; i < pacientes.length; i++) {
