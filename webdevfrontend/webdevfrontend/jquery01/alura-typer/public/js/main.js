9443615f0d743e50610a3943b5920e28f93488a3
Carlos Melo <carlaofernandesdgt@gmail.com>
2018-11-16 09:53:33 -0200

    Aula 3 cronometro Jquery

--- webdevfrontend/webdevfrontend/jquery01/alura-typer/public/js/main.js
+++ webdevfrontend/webdevfrontend/jquery01/alura-typer/public/js/main.js
@@ -6,6 +6,12 @@ function init()
   bindAll();
 }
 
+function bindAll()
+{
+    $(".campo-digitacao").on("input",atualizaContadoresCampoDigitado);
+    $(".campo-digitacao").one("focus",inicializaConometro);
+}
+
 function atualizaContadoresTexto()
 {
     var frase = $(".frase").text();
@@ -13,11 +19,22 @@ function atualizaContadoresTexto()
     $("#tamannho-frase").text(numeroPalavras);
 }
 
-
-function bindAll()
-{
-    $(".campo-digitacao").on("input",atualizaContadoresCampoDigitado);
+function inicializaConometro() {
+    var tempoDigitacao = $("#tempo-digitacao");
+    var tempoRestante = tempoDigitacao.text();
+    const intervaloExecucao = 1000;
+    var cronometroID = setInterval(() => {
+        tempoRestante--;
+        tempoDigitacao.text(tempoRestante);
+        if (tempoRestante < 1)
+        {
+            clearInterval(cronometroID);
+            $(".campo-digitacao").attr("disabled",true);
+        }
+    }, intervaloExecucao);
 }
+
+
 
 function atualizaContadoresCampoDigitado() {
     var campoDigitado = $(this).val();
