e108433932d95f1192b0bdec4b1f2b31bf4eae40
Carlos Melo <carlaofernandesdgt@gmail.com>
2018-11-18 22:48:55 -0200

    Termino Aula 5 Estlizacao com Materialize

--- webdevfrontend/webdevfrontend/jquery01/alura-typer/public/js/main.js
+++ webdevfrontend/webdevfrontend/jquery01/alura-typer/public/js/main.js
@@ -4,18 +4,61 @@ function init()
 {
   atualizaContadoresTexto();
   bindAll();
+  unbindBotaoReiniciar();
 }
 
 function bindAll()
 {
-    $(".campo-digitacao").on("input",atualizaContadoresCampoDigitado);
+    $(".campo-digitacao").on("input",atualizaContadoresEBordas);
     bindCampoDigitacaoCronometro();
-    $("#botao-reiniciar").click(reiniciaJogo);
+}
+
+function bindBotaoReiniciar()
+{
+    $("#botao-reiniciar").on("click",reiniciaJogo);
+}
+
+function unbindBotaoReiniciar()
+{
+    $("#botao-reiniciar").off("click");
+}
+
+function atualizaContadoresEBordas()
+{
+    atualizaContadoresCampoDigitado();
+    atualizaBordaCampoDigitado();
 }
 
 function bindCampoDigitacaoCronometro()
 {
     $(".campo-digitacao").one("focus",inicializaConometro);
+}
+
+function atualizaBordaCampoDigitado()
+{
+    var campoDigitado = $(".campo-digitacao");
+    var valordigitado = campoDigitado.val();
+    var frase = $(".frase").text();
+    if (valordigitado.length > 0)
+    { 
+        //if (valordigitado == frase.substr(0,valordigitado.length))
+        if (frase.startsWith(valordigitado))
+        
+        {
+            campoDigitado.removeClass("borda-valorerrado");
+            campoDigitado.addClass("borda-valorcorreto");
+        }
+        else 
+        {
+            campoDigitado.removeClass("borda-valorcorreto");
+            campoDigitado.addClass("borda-valorerrado");
+        }
+    }
+    else 
+    {
+        campoDigitado.removeClass("borda-valorcorreto");
+        campoDigitado.removeClass("borda-valorerrado");
+    }
 }
 
 function atualizaContadoresTexto()
@@ -25,9 +68,19 @@ function atualizaContadoresTexto()
     $("#tamanho-frase").text(numeroPalavras);
 }
 
+function atualizaContadoresCampoDigitado() {
+    var campoDigitado = $(".campo-digitacao").val();
+    // var numeroPalavras = campoDigitado.split(" ").length;
+    // var numeroCaracteres = campoDigitado.length;
+    //Aplicando melhoria com Expressao Regular 
+    var numeroPalavras = campoDigitado.split(/\S+/).length -1;
+    var numeroCaracteres = campoDigitado.replace(/\s+/g,'').length;
+    $("#contador-palavras").text(numeroPalavras);
+    $("#contador-caracteres").text(numeroCaracteres);
+}
 function inicializaConometro() {
     var tempoDigitacao = $("#tempo-digitacao");
-    $("#botao-reiniciar").attr("disabled",true);
+    //$("#botao-reiniciar").attr("disabled",true);
     var tempoRestante = tempoDigitacao.text();
     const intervaloExecucao = 1000;
     var cronometroID = setInterval(() => {
@@ -37,20 +90,11 @@ function inicializaConometro() {
         {
             clearInterval(cronometroID);
             $(".campo-digitacao").attr("disabled",true);
-            $("#botao-reiniciar").attr("disabled",false);
+            //$("#botao-reiniciar").attr("disabled",false);
+            bindBotaoReiniciar();
+            $(".campo-digitacao").toggleClass("campo-desabilitado");
         }
     }, intervaloExecucao);
-}
-
-function atualizaContadoresCampoDigitado() {
-    var campoDigitado = $(this).val();
-    // var numeroPalavras = campoDigitado.split(" ").length;
-    // var numeroCaracteres = campoDigitado.length;
-    //Aplicando melhoria com Expressao Regular 
-    var numeroPalavras = campoDigitado.split(/\S+/).length -1;
-    var numeroCaracteres = campoDigitado.replace(/\s+/g,'').length;
-    $("#contador-palavras").text(numeroPalavras);
-    $("#contador-caracteres").text(numeroCaracteres);
 }
 
 function reiniciaJogo()
@@ -59,10 +103,14 @@ function reiniciaJogo()
     var numeroCaracteres = 0;
     var campoDigitacao = $(".campo-digitacao");
     campoDigitacao.attr("disabled",false);
+    campoDigitacao.toggleClass("campo-desabilitado");
+    campoDigitacao.removeClass("borda-valorcorreto");
+    campoDigitacao.removeClass("borda-valorerrado");
     campoDigitacao.val("");
     $("#contador-palavras").text(numeroPalavras);
     $("#contador-caracteres").text(numeroCaracteres);
     $("#tempo-digitacao").text(tempoInicial);
+    
     bindCampoDigitacaoCronometro();
 }
 
